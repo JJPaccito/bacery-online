22303d34744a51ccd527b6e4d243fff17f04b488
JJPaccito <33262487+JJPaccito@users.noreply.github.com>
2017-11-13 21:36:42 +0200

    homework

--- app/view/data-table.php
+++ app/view/data-table.php
@@ -5,8 +5,17 @@ $days = $keys ='';
 	foreach ($data as $key => $value) {
 		$days .= "<th colspan=\"5\">$key</th>";
 		$keys .= "<th>VL</th><th>PG</th><th>PR</th><th>SG</th><th>GL</th>";
-	}
 
+		foreach ($value as $name => $data) {
+			# code...
+		// name         //data
+	//	"Agouninė" => [5, 20, 19, 4, 2],
+
+	//		foreach ($data as $amount) {			//nd
+				# code...
+			}
+		}
+	}
 ?>
 
 <table>
